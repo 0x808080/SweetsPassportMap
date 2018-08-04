03c5de994ae1e7219085b6a0c6567ac03e3e6a45
user <user@hotmail.com>
2018-08-04 17:26:50 +0900

    ベース部分の作成

--- app/src/main/java/com/cafekoro/mapapp/MapsActivity.java
+++ app/src/main/java/com/cafekoro/mapapp/MapsActivity.java
@@ -12,12 +12,17 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
+    private final int MAX_MAP_MARKER = 9;                           /**< マーカーの数 */
+    private GoogleMap mMap;                                         /**< googleMap */
+    private MapMarker[] mapMarker = new MapMarker[MAX_MAP_MARKER];  /**< マーカーの座標 */
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        initMapMarker();
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
@@ -38,9 +43,72 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        for( int i = 0; i < MAX_MAP_MARKER; i++ ){
+            MapMarker marker = mapMarker[i];
+            LatLng sydney = new LatLng(marker.x, marker.y);
+            mMap.addMarker(new MarkerOptions().position(sydney).title(marker.name));
+        }
+
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+
+    /**
+     * マーカーの初期化
+     */
+    private void initMapMarker() {
+
+        for( int i = 0; i < MAX_MAP_MARKER; i++ ) {
+            mapMarker[i] = new MapMarker();
+            mapMarker[i].name = Name[i];
+            mapMarker[i].x = point[i][0];
+            mapMarker[i].y = point[i][1];
+        }
+    }
+
+    /**
+     * マーカー座標のクラス
+     */
+    public class MapMarker {
+        public String name;
+        public float x;
+        public float y;
+    }
+
+
+    private String[] Name = {
+            "コッペdeサンド",
+            "TENN.",
+            "FIN'S",
+            "maison de ble'",
+            "安田製パン所",
+            "パンのコマチ 寒河江店",
+            "ぱん工房 obata",
+            "米粉パンのお店 あおいそら",
+            "木村家本店",
+
+            "--",
+            "--",
+    };
+
+    private float[][] point = {
+            { (float) 38.243593, (float) 140.319377 },
+            { (float) 38.288123, (float) 140.321372 },
+            { (float) 38.26259179, (float) 140.32843702 },
+            { (float) 38.248241, (float) 140.338755 },
+            { (float) 37.90498214, (float) 140.126192 },    // ここから上は不確定情報
+            { (float) 38.380743, (float) 140.263422 },
+            { (float) 00.000000, (float) 000.000000 },
+            { (float) 38.576873, (float) 140.383165 },
+            { (float) 38.107174, (float) 140.037502 },      // ここまでパン
+
+            { (float) 00.000000, (float) 000.000000 },
+            { (float) 00.000000, (float) 000.000000 },
+            { (float) 00.000000, (float) 000.000000 },
+    };
 }
+
+
+
+
+
+
